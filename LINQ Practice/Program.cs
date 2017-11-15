cf4d8e3b4d4a35f68ea3fbb18397f9c33004af15
kevingilfether <kevingilfether@gmail.com>
2017-11-15 13:32:04 -0500

    Initial Commit

---
+++ LINQ Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "hi", "LINQ", "curious", "Transformer", "why" };
+
+            //LINQ to query short words
+
+            var shortWords = from word in words where word.Length <= 5 select word;
+
+            // iterate through LINQ result set
+
+            foreach (var word in shortWords)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
